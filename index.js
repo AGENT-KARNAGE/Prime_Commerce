2d89afe79c73264d95920a49788c4f4099064124
AGENT-KARNAGE <omobowaleosinowo@gmail.com>
2024-03-06 11:46:53 +0100

    Set up index file

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+
+//initialize an express object
+const  app = express()
+
+//for json data
+app.use(express.json())
+
+//port
+const port = process.env.PORT || 5890
+
+//connect
+app.listen(PORT, () => {
+    console.log(`server is running on port http://localhost:$PORT`)
+})
