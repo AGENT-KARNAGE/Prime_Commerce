e23478fbe78d7b9cc115e7e67c482eaca745343d
AGENT-KARNAGE <omobowaleosinowo@gmail.com>
2024-03-06 12:28:09 +0100

    MongoDB setup

---
+++ config/dbConfig.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const connectDB = async ()  => {
+    try {
+        
+    } catch (error) {
+        console.log(`Error : ${error.message}`)
+    }
+}
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 const express = require('express')
+const { connect } = require('mongoose')
+const connectDB = require('./config/dbConfig')
+const mongoose = require( 'mongoose' )
 
 //initialize an express object
 const  app = express()
@@ -7,10 +10,18 @@ const  app = express()
 //for json data
 app.use(express.json())
 
+//db config
+connectDB()
+
 //port
 const port = process.env.PORT || 5890
 
 //connect
-app.listen(PORT, () => {
-    console.log(`server is running on port http://localhost:$PORT`)
+mongoose.connection.once( 'open', () => {
+    console.log('Database connection success!')
+    
+    
+    app.listen(port, ()=>{
+    console.log(`server is running on port http://localhost:${PORT}`)
+    })
 })
